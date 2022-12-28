d53ece706ceb0b0ccb5f5dfc980c1eb9a62809c8
Crislarr <larroussecristian@outlook.com>
2022-12-27 21:10:00 -0300

    Quitando funcion innecesaria

--- tarea2.js
+++ tarea2.js
@@ -1,24 +1,20 @@
-function saludarUsuario() {
-  const primerNombre = document.querySelector("#primer-nombre-usuario").value;
+const primerNombre = document.querySelector("#primer-nombre-usuario").value;
 
-  let segundoNombre;
-
-  if (!document.querySelector("#segundo-nombre-usuario").value) {
-    segundoNombre = "";
-  } else {
-    segundoNombre = document.querySelector("#segundo-nombre-usuario").value;
-  }
-
-  const apellido = document.querySelector("#apellido-usuario").value;
-
-  const edad = Number(document.querySelector("#edad-usuario").value);
-
-  return `Bienvenido ${primerNombre} ${segundoNombre} ${apellido}, ${edad} años de edad!`;
+let segundoNombre;
+if (!document.querySelector("#segundo-nombre-usuario").value) {
+  segundoNombre = "";
+} else {
+  segundoNombre = document.querySelector("#segundo-nombre-usuario").value;
 }
-console.log(saludarUsuario());
+
+const apellido = document.querySelector("#apellido-usuario").value;
+
+const edad = Number(document.querySelector("#edad-usuario").value);
+
+const saludo = `Bienvenid/a ${primerNombre} ${segundoNombre} ${apellido}, ${edad} años de edad!`;
+
+console.log(saludo);
 
 document.querySelector("#ingresar").onclick = function () {
-  const saludo = saludarUsuario();
-
   document.querySelector("#saludo").textContent = saludo;
 };
